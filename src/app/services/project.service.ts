120ca30056dab38c2e2473001877298544fcb7c6
arcd <alfredocrd@hotmail.com>
2021-10-29 12:00:01 -0300

    BASE CON CODIGO UPLOAD DE VICTOR

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import {HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Project } from "../models/projects";
+import { Global } from "./global"
+
+@Injectable()
+export class ProjectService{
+    public url: string;
+
+    constructor(
+        private _http: HttpClient
+    ){
+        this.url = Global.url;
+    }
+
+    testService(){
+        return "Probando el Servicio de Angluar";
+    }
+
+    saveProject(project: Project): Observable <any>{
+        let params = JSON.stringify(project);
+        let headers = new HttpHeaders().set("Content-Type", "application/json");
+
+        return this._http.post(this.url+'save-project', params, {headers: headers});
+
+    }
+}
